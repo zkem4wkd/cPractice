8a3ce68dce24860931860adc95eaaea966cd771e
zkem4wkd <62642237+zkem4wkd@users.noreply.github.com>
2020-08-19 15:50:26 +0900

    20.08.19

---
+++ LoginPractice/LoginPractice/실습소스3.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <stdio.h>
+
+int main()
+{
+	int number = 0;
+	int number2 = 0;
+
+	printf("Ã¹¹øÂ° ¼ıÀÚ¸¦ ÀÔ·Â : ");
+	scanf("%d",&number);
+	printf("µÎ¹øÂ° ¼ıÀÚ¸¦ ÀÔ·Â : ");
+	scanf("%d",&number2);
+	int plus = number + number2;
+	printf(" µ¡¼À : %d + %d = %d\n", number, number2,plus);
+	int minus = number - number2;
+	printf(" »¬¼À : %d - %d = %d\n", number, number2, minus);
+	int div = number / number2;
+	printf(" ³ª´°¼À : %d / %d = %d\n", number, number2, div);
+	int mul = number * number2;
+	printf(" °ö¼À : %d * %d = %d\n", number, number2, mul);
+	printf(" µ¡¼À : %d + %d = %d\n", number, number2, number+number2);
+	
+	return 0;
+}
