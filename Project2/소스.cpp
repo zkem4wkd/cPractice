17f37c34dd80d30a82f3eb1cdaa014a922813812
zkem4wkd <62642237+zkem4wkd@users.noreply.github.com>
2020-08-19 10:13:40 +0900

    dataType 연습
    1

---
+++ Project2/소스.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void)
+{
+	//char type
+	char Char = 'a';
+	char Hello = 'H';
+
+	printf("%c\n",Char);
+	printf("%c\n", Hello);
+
+	//int type
+	int Attack = 10;
+	int Guard = 10;
+	int MP = 5;
+
+	printf("AP:%d\nGP:%d\nMP:%d\n", Attack, Guard, MP);
+}
