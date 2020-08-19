bbec719ee19ec8112ed33c9716246b3a769dcd60
zkem4wkd <62642237+zkem4wkd@users.noreply.github.com>
2020-08-19 15:02:06 +0900

    20.08.19

--- LoginPractice/LoginPractice/소스.cpp
+++ LoginPractice/LoginPractice/소스.cpp
@@ -53,9 +53,29 @@ int main(void)
 		printf("방어력 : %d \n", Def);
 		printf("=====================================\n");
 	}
-	else
+	else if (select > 3 || select == 0) {
 		printf("올바른 값 입력");
-	
+	}
 
+	int input = 0;
+	int exp = 10;
+	printf("적을 만났다.\n");
+	printf("1. 싸운다 2. 도망간다\n");
+	scanf("%d", &input);
+	Sleep(1000);
+	printf("싸워서 승리했다.\n");
+	printf("경험치가 %d 만큼 올랐다.\n", exp);
+	Sleep(1000);
+	printf(".\n");
+	Sleep(1000);
+	printf("..\n");
+	Sleep(1000);
+	printf("...\n");	
+	printf("레벨이 상승하였다.\n");
+	Sleep(500);
+	Hp = Hp + 8;
+	Atk = Atk + 2;
+	Def = Def + 1;
+	printf("능력치가 상승하였다. \nHP : %d / 공격력 : %d / 방어력 : %d\n", Hp, Atk, Def);
 
 }
