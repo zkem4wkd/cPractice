e67394e536bce6849b63c335e10aac56f90ac573
zkem4wkd <62642237+zkem4wkd@users.noreply.github.com>
2020-08-20 13:20:56 +0900

    20.08.20

---
+++ if/if/소스.cpp
@@ -0,0 +1,104 @@
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+	/*int a ,b;
+	printf("first number a : ");
+	scanf("%d", &a);
+	printf("second number b : ");
+	scanf("%d", &b);
+
+	if (a < b)
+	{
+		printf("a < b ");
+	}
+	else if(a > b)
+	{
+		printf("a > b");
+	}
+	else
+	{
+		printf(" a = b");
+	}*/
+
+	/*int a;
+	printf(" 1.Attack 2.Guard 3.Run \n Enter : ");
+	scanf("%d", &a);
+
+	if (a == 1)
+	{
+		printf("Attack");
+	}
+	else if (a == 2)
+	{
+		printf("Guard");
+	}
+	else if (a == 3)
+	{
+		printf("Run");
+	}
+	else
+	{
+		printf("Error");
+	}*/
+
+	/*int a;
+	printf("Enter a's Value  : ");
+	scanf("%d", &a);
+
+	if(a == 2)
+	{
+		printf("Entered in number 2 ");
+	}
+	else
+	{
+		printf("Entered in number %d",a);
+	}*/
+
+	/*int a = 11;
+	int b = 13;
+
+	if (a == 11 && b == 13)
+	{
+		printf("a is 11 b is 13\n");
+	}
+
+	int c = 15;
+	int d = 20;
+
+	if (c == 15 || d == 20)
+	{
+		printf("c is 15 or d is 20\n");
+	}*/
+
+	/*int walk = 0;
+
+	if (!walk)
+	{
+		printf("character is walking");
+	}*/
+
+	int a;
+	printf("1.파이리 2.꼬부기 3.이상해씨\n");
+	scanf("%d", &a);
+
+	if (a == 1)
+	{
+		printf("파이리");
+	}
+	else if (a == 2)
+	{
+		printf("꼬부기");
+	}
+	else if (a == 3)
+	{
+		printf("이상해씨");
+	}
+	else
+	{
+		printf("Error");
+	}
+}
