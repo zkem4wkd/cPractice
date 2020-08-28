9dbc9c4718e9fb1ad84268c257b0a2552e118914
zkem4wkd <62642237+zkem4wkd@users.noreply.github.com>
2020-08-28 12:15:13 +0900

    20.08.28

---
+++ Enum/소스.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+enum Color
+{
+	Red ,
+	Yellow=3,
+	Blue
+};
+
+
+int main()
+{
+	printf("Color : %d\n", Red);
+	printf("Color : %d\n", Yellow);
+	printf("Color : %d\n", Blue);
+
+	return 0;
+}
