69a39dd88d3018441e11ce9e47c51d8bc8279954
zkem4wkd <62642237+zkem4wkd@users.noreply.github.com>
2020-08-28 11:18:32 +0900

    20.08.28

--- Text Game Practice/소스.cpp
+++ Text Game Practice/소스.cpp
@@ -23,11 +23,41 @@ char Monster2_Name[40] = "Worm";
 char Monster3_Name[40] = "Kobolt";
 char Monster4_Name[40] = "Flog";
 char Monster5_Name[40] = "JellyBee";
+int dropItem = 0;
 
+
+char Bat_Item1[40] = "Bat's Wing";
+char Bat_Item2[40] = "Bat's tusk";
+char Worm_Item1[40] = "Worm's skin";
+char Worm_Item2[40] = "Acid Liquid";
+char Kobolt_Item1[40] = "Old leather";
+char Kobolt_Item2[40] = "Kobolt's knife";
+char Flog_Item1[40] = "Flog's tongue";
+char Flog_Item2[40] = "Slippery mucus";
+char JellyBee_Item1[40] = "Yellow Jelly";
+char JellyBee_Item2[40] = "JellyBee's stone";
+
+
+char pInven1[50] = {'\0'};
+int item_amount1 = 0;
+char pInven2[50] = { '\0' };
+int item_amount2 = 0;
+char pInven3[50] = { '\0' };
+int item_amount3 = 0;
+char pInven4[50] = { '\0' };
+int item_amount4 = 0;
+char pInven5[50] = { '\0' };
+int item_amount5 = 0;
+
+int dInside=0;
+int dLv=0;
 void dStart();
 void Encount_Monster();
 void dBattle();
-
+void Death();
+void Heal();
+void DropItem();
+void Get_Item();
 
 int main()
 {
@@ -73,6 +103,7 @@ int main()
 
 	dStart();
 
+
 	return 0;
 }
 
@@ -86,8 +117,14 @@ void dStart()
 
 	printf("--------------------------------------------\n");
 
-	printf("Enter the dungeon\n1.Enter 2.Exit\n");
-
+	if (dInside==0)
+	{
+		printf("Enter the dungeon\n1.Enter 2.Exit 3.Heal \n");
+	}
+	else if (dInside!=0)
+	{
+		printf("Enter the Deep Space\n1.Enter 2.Exit 3.Heal");
+	}
 	scanf_s("%d", &D_Plag);
 
 	switch (D_Plag)
@@ -98,6 +135,10 @@ void dStart()
 	case 2:
 		printf("Game End\n");
 		Sleep(500);
+		exit(0);
+		break;
+	case 3:
+		Heal();
 		break;
 	default :
 		printf("Command Error");
@@ -110,9 +151,11 @@ void dStart()
 void Encount_Monster()
 {
 	system("cls");
-	printf("You arrived Dungeon\n");
+	printf("You arrived Dungeon total : %d \n",dLv);
+	dInside = 1;
+	dLv++;
 
-	Sleep(2000);
+	Sleep(1000);
 
 	system("cls");
 
@@ -203,6 +246,7 @@ void dBattle()
 			system("cls");
 			dBattle();
 		}
+		Death();
 		Player_turn = 0;
 	}
 	Sleep(800);
@@ -235,4 +279,231 @@ void dBattle()
 	Player_turn = 1;
 
 	dBattle();
+}
+
+void Death()
+{
+	if (P_Hp <= 0)
+	{
+		Sleep(500);
+		system("cls");
+		printf("You dead .\n");
+		dLv = 0;
+		Sleep(500);
+		exit(0);
+	}
+	if (M_Hp <= 0)
+	{
+		Sleep(500);
+		system("cls");
+		printf("You Slayed %s\n",Monster_Name);
+		Sleep(700);
+		DropItem();
+		printf("Battle end");
+		Sleep(1000);
+		dStart();
+	}
+	
+}
+
+void Heal()
+{
+	system("cls");
+	printf("You healed by Church.\n");
+	dInside = 0;
+	dLv = 0;
+	Sleep(500);
+
+	if (Job_Plag == 1)
+	{
+		P_Hp = 100;
+	}
+	else if (Job_Plag == 2)
+	{
+		P_Hp = 80;
+	}
+	else if (Job_Plag == 3)
+	{
+		P_Hp = 60;
+	}
+	dStart();
+}
+
+void DropItem()
+{
+	if (EncountRand == 1)
+	{
+		for (int i = 0; i < 3; i++)
+		{
+			dropItem = rand() % 3;
+			if (dropItem == 0)
+			{
+				printf("You gain nothing.\n");
+			}
+			else if (dropItem == 1)
+			{
+				printf("You get %s\n", Bat_Item1);
+				Get_Item(Bat_Item1);
+			}
+			else if (dropItem == 2)
+			{
+				printf("You get %s\n", Bat_Item2);
+				Get_Item(Bat_Item2);
+			}
+		}
+	}
+	if (EncountRand == 2)
+	{
+		for (int i = 0; i < 3; i++)
+		{
+			dropItem = rand() % 3;
+			if (dropItem == 0)
+			{
+				printf("You gain nothing.\n");
+			}
+			else if (dropItem == 1)
+			{
+				printf("You get %s\n", Worm_Item1);
+				Get_Item(Worm_Item1);
+			}
+			else if (dropItem == 2)
+			{
+				printf("You get %s\n", Worm_Item2);
+				Get_Item(Worm_Item2);
+			}
+		}
+	}
+	if (EncountRand == 3)
+	{
+		for (int i = 0; i < 3; i++)
+		{
+			dropItem = rand() % 3;
+			if (dropItem == 0)
+			{
+				printf("You gain nothing.\n");
+			}
+			else if (dropItem == 1)
+			{
+				printf("You get %s\n", Kobolt_Item1);
+				Get_Item(Kobolt_Item1);
+			}
+			else if (dropItem == 2)
+			{
+				printf("You get %s\n", Kobolt_Item2);
+				Get_Item(Kobolt_Item2);
+			}
+		}
+	}
+	if (EncountRand == 4)
+	{
+		for (int i = 0; i < 3; i++)
+		{
+			dropItem = rand() % 3;
+			if (dropItem == 0)
+			{
+				printf("You gain nothing.\n");
+			}
+			else if (dropItem == 1)
+			{
+				printf("You get %s\n", Flog_Item1);
+				Get_Item(Flog_Item1);
+			}
+			else if (dropItem == 2)
+			{
+				printf("You get %s\n", Flog_Item2);
+				Get_Item(Flog_Item2);
+			}
+		}
+	}
+	if (EncountRand == 5)
+	{
+		for (int i = 0; i < 3; i++)
+		{
+			dropItem = rand() % 3;
+			if (dropItem == 0)
+			{
+				printf("You gain nothing.\n");
+			}
+			else if (dropItem == 1)
+			{
+				printf("You get %s\n", JellyBee_Item1);
+				Get_Item(JellyBee_Item1);
+			}
+			else if (dropItem == 2)
+			{
+				printf("You get %s\n", JellyBee_Item2);
+				Get_Item(JellyBee_Item2);
+			}
+		}
+	}
+}
+
+void Get_Item(char getItem[50])
+{
+	if (!strcmp(pInven1, getItem))
+	{
+		item_amount1++;
+		printf("%s is duplicated, Total : %d \n",pInven1,item_amount1);
+		return;
+	}
+	if (!strcmp(pInven2, getItem))
+	{
+		item_amount2++;
+		printf("%s is duplicated, Total : %d \n", pInven2, item_amount2);
+		return;
+	}
+	if (!strcmp(pInven3, getItem))
+	{
+		item_amount3++;
+		printf("%s is duplicated, Total : %d \n", pInven3, item_amount3);
+		return;
+	}
+	if (!strcmp(pInven4, getItem))
+	{
+		item_amount4++;
+		printf("%s is duplicated, Total : %d \n", pInven4, item_amount4);
+		return;
+	}
+	if (!strcmp(pInven5, getItem))
+	{
+		item_amount5++;
+		printf("%s is duplicated, Total : %d \n", pInven5, item_amount5);
+		return;
+	}
+
+	if (!strcmp(pInven1, "")) //empty pInven
+	{
+		strcpy_s(pInven1,strlen(getItem)+1, getItem);
+
+		item_amount1++;
+	}
+	else if(!strcmp(pInven2, "")) 
+	{
+		strcpy_s(pInven2, strlen(getItem) + 1, getItem);
+
+		item_amount2++;
+	}
+	else if (!strcmp(pInven3, ""))
+	{
+		strcpy_s(pInven3, strlen(getItem) + 1, getItem);
+
+		item_amount3++;
+	}
+	else if (!strcmp(pInven4, ""))
+	{
+		strcpy_s(pInven4, strlen(getItem) + 1, getItem);
+
+		item_amount4++;
+	}
+	else if (!strcmp(pInven5, ""))
+	{
+		strcpy_s(pInven5, strlen(getItem) + 1, getItem);
+
+		item_amount5++;
+	}
+	else
+	{
+		printf("Inventory is full. You can't has items.\n");
+	}
+	printf("Inventory\n[1]%s\n[2]%s\n[3]%s\n[4]%s\n[5]%s\n", pInven1, pInven2, pInven3, pInven4, pInven5);
 }
